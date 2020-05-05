d927f68f645094ec56456f7ca3622e1a5eeb0515
LeonidK23 <kaida.leonid@gmail.com>
2020-05-05 12:28:57 +0200

    change namestyle

--- src/ImageReader.cpp
+++ src/ImageReader.cpp
@@ -5,21 +5,25 @@
 #include "Image.hpp"
 #include "ImageReader.hpp"
 
-Image ImageReader::read(std::string file_path){
-  Image img{};
+Image ImageReader::read(std::string filePath){
   int num;
+  int filenameBegin;
+  int filenameEnd;
+  std::string fileName;
+  Image img{};
 
-  std::ifstream inputFile(file_path);
+  std::ifstream inputFile(filePath);
   std::vector<int> data;
 
   while (inputFile >> num){
     data.push_back(num);
   }
 
-  int filename_begin = file_path.find_last_of("/\\") + 1;
-  int filename_end = file_path.find_last_of(".");
-  std::string file_name = file_path.substr(filename_begin, filename_end - filename_begin);
-  img.setName(file_name);
+  filenameBegin = filePath.find_last_of("/\\") + 1;
+  filenameEnd = filePath.find_last_of(".");
+  fileName = filePath.substr(filenameBegin, filenameEnd - filenameBegin);
+
+  img.setName(fileName);
   img.setData(data);
 
   return img;
