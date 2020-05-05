eb746bc3f5fcbab7f552dbfc8c7a7b35d66069d9
LeonidK23 <kaida.leonid@gmail.com>
2020-05-05 12:19:15 +0200

    add tests for image reading

---
+++ tests/main_test.cpp
@@ -0,0 +1,16 @@
+#define CATCH_CONFIG_MAIN
+
+#include <iostream>
+
+#include "catch.hpp"
+#include "Image.hpp"
+#include "ImageReader.hpp"
+
+TEST_CASE("Image Reader and Image Classes", ""){
+  Image im{};
+  ImageReader reader{};
+  im = reader.read("../data/test.txt");
+
+  REQUIRE(im.getData() == std::vector<int> {89, 31, 25, 54, 1, 9, 100, 254, 198});
+  REQUIRE(im.getName() == "test");
+}
